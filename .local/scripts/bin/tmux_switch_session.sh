b22e4788b380a57d37f80246b699f6f8501f65bd
Manuel Luna <manuellunada@gmail.com>
2024-10-12 17:54:38 -0600

    fix tmux switch script

--- .local/scripts/bin/tmux_switch_session.sh
+++ .local/scripts/bin/tmux_switch_session.sh
@@ -3,9 +3,12 @@
 output=$(tmux list-windows -a -F "#{session_id} #{window_id} #{session_name}: #{window_name}" | \
 	fzf --with-nth=3,4)
 
-[ -z "$output" ] && return
+[ -z "$output" ] && exit
 
 session=$(awk '{print $1}' <<< "$output")
 window=$(awk '{print $2}' <<< "$output")
+
+[ -z "$session" ] && exit
+[ -z "$window" ] && exit
 
 tmux switch-client -t "$session" \; select-window -t "$window"
