dff720571d2742d320116435cb1c874ee46da77b
Manuel Luna <manuellunada@gmail.com>
2024-10-15 10:49:30 -0600

    nvim fix oil nil error

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,14 +13,15 @@ vim.opt.cursorline = false
 vim.opt.hlsearch = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.laststatus = 3
 
 -- PLUGINS
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -43,21 +44,21 @@ require("lazy").setup({
       vim.cmd("colorscheme kanagawa-dragon")
     end
   },
-	{
-		"nvim-telescope/telescope.nvim",
-		event = "VimEnter",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-		},
-		config = function()
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+    },
+    config = function()
       local telescope = require("telescope")
       local telescopeConfig = require("telescope.config")
 
@@ -88,18 +89,20 @@ require("lazy").setup({
         },
       })
 
-			pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "fzf")
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-			vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find expression" })
-			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "File history" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
-		end,
-	},
-	{
-		"stevearc/oil.nvim",
-		config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find expression" })
+      vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "File history" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      local oil = require("oil")
+
       require("oil").setup({
         keymaps = {
           ["g?"] = "actions.show_help",
@@ -159,163 +162,163 @@ require("lazy").setup({
         skip_confirm_for_simple_edits = true,
       })
 
-			vim.keymap.set("n", "<leader>fn", "<cmd>Oil<cr>", { desc = "File explorer" })
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{
-				"L3MON4D3/LuaSnip",
-				build = (function()
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {},
-			},
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
+      vim.keymap.set("n", "<leader>fn", "<cmd>Oil<cr>", { desc = "File explorer" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        build = (function()
+          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+            return
+          end
+          return "make install_jsregexp"
+        end)(),
+        dependencies = {},
+      },
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup({})
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
 
-					["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-					["<C-k>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping.confirm({ select = true }),
 
-					["<C-Space>"] = cmp.mapping.complete({}),
+          ["<C-Space>"] = cmp.mapping.complete({}),
 
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
 
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-				callback = function(event)
-					local telescope = require("telescope.builtin")
-					vim.keymap.set("n", "<leader>lu", telescope.lsp_definitions, { desc = "Search definitions" })
-					vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { desc = "Code actions" })
-					vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Documentation" })
-					vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
-					vim.keymap.set("n", "<leader>lf", vim.lsp.buf.definition, { desc = "Go to definition" })
-					vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Diagnostics" })
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+        callback = function(event)
+          local telescope = require("telescope.builtin")
+          vim.keymap.set("n", "<leader>lu", telescope.lsp_definitions, { desc = "Search definitions" })
+          vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { desc = "Code actions" })
+          vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Documentation" })
+          vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
+          vim.keymap.set("n", "<leader>lf", vim.lsp.buf.definition, { desc = "Go to definition" })
+          vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Diagnostics" })
           vim.keymap.set("i", "<c-s>", vim.lsp.buf.signature_help, { desc = "Signature" })
-				end,
-			})
+        end,
+      })
 
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			local servers = {
-				gopls = {},
+      local servers = {
+        gopls = {},
         elixirls = {},
         golangci_lint_ls = {},
         pyright = {},
         bashls = {},
         tsserver = {},
         cssls = {},
-			}
+      }
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		config = function()
-			require("conform").setup({
-				notify_on_error = true,
-				format_on_save = false,
-				formatters_by_ft = {
-					go = { "goimports" },
-					lua = { "stylua" },
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            require("lspconfig")[server_name].setup(server)
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    lazy = false,
+    config = function()
+      require("conform").setup({
+        notify_on_error = true,
+        format_on_save = false,
+        formatters_by_ft = {
+          go = { "goimports" },
+          lua = { "stylua" },
           elixir = { "mix" },
-				},
-			})
-			vim.api.nvim_create_user_command("W", function()
-				require("conform").format({
-					bufnr = vim.api.nvim_get_current_buf(),
-				})
-			end, {})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
+        },
+      })
+      vim.api.nvim_create_user_command("W", function()
+        require("conform").format({
+          bufnr = vim.api.nvim_get_current_buf(),
+        })
+      end, {})
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
         "lua",
         "go",
         "elixir",
         "bash",
       },
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.install").prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.install").prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
   {
     -- yes, i need this
     "karb94/neoscroll.nvim",
