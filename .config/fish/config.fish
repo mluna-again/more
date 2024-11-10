74281075d05a45c38dea371a5191e63e5ed99534
Manuel Luna <manuellunada@gmail.com>
2024-11-10 17:15:38 -0600

    kitty ssh abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,6 +67,10 @@ abbr --add setup-idf source "$HOME/.local/esp/esp-idf/export.fish"
 abbr --add esp idf.py
 abbr --add rsync rsync -avh --info=progress2
 
+if test "$TERM" = xterm-kitty
+  abbr --add ssh kitten ssh
+end
+
 set fish_cursor_default block
 set fish_cursor_insert block
 set fish_cursor_replace_one underscore
