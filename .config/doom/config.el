d06e4c747761c4fc2639f59268bd80eba4db8d03
Manuel Luna <manuellunada@gmail.com>
2024-12-03 12:21:55 -0600

    emacs config

---
+++ .config/doom/config.el
@@ -0,0 +1,178 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+
+ (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-gruvbox)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Org/")
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(setq-default evil-escape-key-sequence "jj")
+
+(setq org-startup-with-inline-images t)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+(add-hook 'org-mode-hook (lambda ()
+                           (setq fill-column 120)
+                           (turn-on-auto-fill)))
+(setq org-superstar-headline-bullets-list
+      '("󰴈" "" "󰫢" ""))
+
+(setq org-roam-directory (file-truename "~/Org"))
+(map! "C-SPC" #'completion-at-point)
+(map! "C-c n l" #'org-roam-buffer-toggle)
+(map! "C-c n f" #'org-roam-node-find)
+(map! "C-c n g" #'org-roam-graph)
+(map! "C-c n i" #'org-roam-node-insert)
+(map! "C-c n c" #'org-roam-capture)
+(map! "C-c n j" #'org-roam-dailies-capture-today)
+(map! "C-c ¿" #'org-table-edit-field)
+(map! :leader :n "f b" #'switch-to-buffer)
+(map! :mode 'org-mode "C-c TAB" #'org-table-toggle-column-width)
+(map! :ni "C-w o" #'delete-other-windows
+      :ni "C-w o" #'delete-other-windows)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "<return>") nil)
+  (define-key company-active-map (kbd "<tab>") nil)
+  (define-key company-active-map (kbd "RET") nil)
+  (define-key company-active-map (kbd "C-k") #'company-complete-selection))
+
+(setq org-timer-done-hook
+      (lambda()
+        (message-box "   Go away   ")))
+(setq org-clock-sound (file-truename "~/.config/doom/bell.wav"))
+(setq org-timer-default-timer "00:15:00")
+(setq org-html-table-caption-above nil)
+(mouse-avoidance-mode 'cat-and-mouse)
+
+(setq treemacs-width 50)
+(setq treemacs-position 'right)
+
+(with-eval-after-load 'ispell
+  (condition-case nil
+      (progn
+        (setq ispell-program-name "hunspell")
+        (setq ispell-dictionary "en_US,es_ES")
+        (ispell-set-spellchecker-params)
+        (ispell-hunspell-add-multi-dic "en_US,es_ES"))
+    (error "Could not load hunspell")))
+
+(use-package! evil-terminal-cursor-changer
+  :hook (tty-setup . evil-terminal-cursor-changer-activate))
+
+(setq browse-url-browser-function #'browse-url-firefox)
+(setenv "BROWSER" "firefox")
+
+(defun recalculate-tables ()
+  "Recalculates all tables in the current Org buffer."
+  (interactive)
+  (if (string= major-mode "org-mode")
+      (progn
+        ;; (org-table-recalculate-buffer-tables)
+        (org-table-iterate-buffer-tables)
+        (message
+         (format-time-string "Last updated: %d/%m/%Y at %H:%M %p")))
+    (message "Not an Org file")))
+
+
+(unless (display-graphic-p)
+  ;; activate mouse-based scrolling
+  (xterm-mouse-mode 1)
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+
+(defun open-ledger-journal ()
+  (interactive)
+  (find-file "~/Ledger/journal.ledger"))
+
+(defun open-config-file ()
+  (interactive)
+  (find-file "~/.config/doom/config.el"))
+
+(map! :leader :n "o c" #'open-config-file)
+(map! :leader :n "o l" #'open-ledger-journal)
+
+(add-to-list '+doom-dashboard-menu-sections
+             '("Open Ledger Journal"
+               :key "SPC o l"
+               :icon (nerd-icons-faicon "nf-fa-book" :face 'doom-dashboard-menu-title)
+               :action open-ledger-journal))
+
+(add-to-list '+doom-dashboard-menu-sections
+             '("Open config file"
+               :key "SPC o c"
+               :icon (nerd-icons-sucicon "nf-seti-config" :face 'doom-dashboard-menu-title)
+               :action open-config-file))
+
+(custom-theme-set-faces! 'doom-gruvbox
+  '(default :background "#181816"))
