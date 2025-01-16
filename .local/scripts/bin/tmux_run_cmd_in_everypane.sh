481b066cee4d3b28575d1b093edda95f4748cac9
Manuel Luna <manuellunada@gmail.com>
2025-01-16 13:16:16 -0600

    tmux run cmd in every pane bind

---
+++ .local/scripts/bin/tmux_run_cmd_in_everypane.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+_SHELLS="bash fish sh zsh"
+
+cmd=$(tmux command-prompt 'display -p "%%%"')
+[ -z "$cmd" ] && exit 1
+
+while read -r pane; do
+  id="$(awk '{print $1}' <<< "$pane")"
+  shell="$(awk '{print $2}' <<< "$pane")"
+  if grep -qvi "$shell" <<< "$_SHELLS"; then
+    continue
+  fi
+
+  tmux send-keys -t "$id" "$cmd" Enter
+done < <(tmux list-panes -F "#{pane_id} #{pane_current_command}")
