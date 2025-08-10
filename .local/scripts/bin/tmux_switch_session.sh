180a1e923a9ce28ff1e93226faab4b39f5a153c5
Manuel Luna <manuellunada@gmail.com>
2025-08-09 19:51:01 -0600

    fzf make it pretty

--- .local/scripts/bin/tmux_switch_session.sh
+++ .local/scripts/bin/tmux_switch_session.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 output=$(tmux list-windows -a -F "#{session_id} #{window_id} #{session_name}: #{window_name}" | \
-	fzf --with-nth=3,4)
+	fzf --with-nth=3,4 --ghost="Search a TMUX session")
 
 [ -z "$output" ] && exit
 
