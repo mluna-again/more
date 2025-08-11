272525b93a3ae688e5b3067a0a26a067690cf64f
Manuel Luna <manuellunada@gmail.com>
2025-08-11 14:24:14 -0600

    tmux spotify_player

--- .local/scripts/bin/tmux_run_menu.sh
+++ .local/scripts/bin/tmux_run_menu.sh
@@ -5,5 +5,6 @@ options="${options} 'Stop applications' S 'run-shell ~/.local/scripts/bin/tmux_s
 options="${options} 'Run command' r 'run-shell ~/.local/scripts/bin/tmux_run_cmd_in_everypane.sh'"
 options="${options} 'Send Keys' k 'run-shell ~/.local/scripts/bin/tmux_sendkeys_everypane.sh'"
 options="${options} 'Clear panes' C 'run-shell ~/.local/scripts/bin/tmux_clear_everypane.sh'"
+options="${options} 'Spotify' m 'run-shell ~/.local/scripts/bin/tmux_spotify.sh'"
 
 eval tmux display-menu -T " Runner " -x C -y "10%" "$options"
--- .local/scripts/bin/tmux_spotify.sh
+++ .local/scripts/bin/tmux_spotify.sh
@@ -4,13 +4,23 @@
 # Clone shinobu and follow the instructions below:
 # https://github.com/mluna-again/shinobu/tree/master/.local/scripts/bop
 
-id=$(docker container ls | awk '$0 ~ "bop-server" { print $1 }')
-if [ -z "$id" ]; then
-  tmux display-message -d 0 "could not find bop, is it running? (check tmux_spotify.sh for details)"
+# bop is tired :(
+# id=$(docker container ls | awk '$0 ~ "bop-server" { print $1 }')
+# if [ -z "$id" ]; then
+#   tmux display-message -d 0 "could not find bop, is it running? (check tmux_spotify.sh for details)"
+#   exit 0
+# fi
+#
+# options="'Player' m 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui player'"
+# options="${options} 'Selector' s 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui select'"
+# options="${options} 'Queue' q 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui queue'"
+# eval tmux display-menu -T " Runner " -x C -y "10%" "$options"
+
+if ! command -v spotify_player &>/dev/null; then
+  tmux display-message "spotify_player is not installed. Remember to run spotify_player authenticate afterwards."
   exit 0
 fi
 
-options="'Player' m 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui player'"
-options="${options} 'Selector' s 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui select'"
-options="${options} 'Queue' q 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui queue'"
-eval tmux display-menu -T " Runner " -x C -y "10%" "$options"
+tmux display-popup -w 95% -h 95% -y S -EE spotify_player
+
+true
