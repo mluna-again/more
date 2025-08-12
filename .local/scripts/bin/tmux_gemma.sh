037c2cd26885badfbd70f77c5c88c8260999ef53
Manuel Luna <manuellunada@gmail.com>
2025-08-11 21:47:55 -0600

    tmux gemma

---
+++ .local/scripts/bin/tmux_gemma.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+tmux display-popup -EE ollama run gemma3
--- .local/scripts/bin/tmux_run_menu.sh
+++ .local/scripts/bin/tmux_run_menu.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-options="'Start applications' s 'run-shell ~/.local/scripts/bin/tmux_start_apps.sh'"
+options="'Gemma' g 'run-shell ~/.local/scripts/bin/tmux_gemma.sh'"
+options="${options} 'Start applications' s 'run-shell ~/.local/scripts/bin/tmux_start_apps.sh'"
 options="${options} 'Stop applications' S 'run-shell ~/.local/scripts/bin/tmux_stop_apps.sh'"
 options="${options} 'Run command' r 'run-shell ~/.local/scripts/bin/tmux_run_cmd_in_everypane.sh'"
 options="${options} 'Send Keys' k 'run-shell ~/.local/scripts/bin/tmux_sendkeys_everypane.sh'"
