961f236e497959161c47ddcb9bc2ad75205ef2f7
Manuel Luna <manuellunada@gmail.com>
2025-08-16 11:54:05 -0600

    tmux_swap_pane.sh

---
+++ .local/scripts/bin/tmux_swap_pane.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+source=$(tmux display -p -F '#{pane_id} #{pane_active}' | awk '$2 == "1" {print $1}' | xargs) || exit
+target=$(tmux display-panes -d 0 'display -p %%' | xargs) || exit
+[ -z "$target" ] && exit 0
+
+tmux swap-pane -s "$source" -t "$target"
