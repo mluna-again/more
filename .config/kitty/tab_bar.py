ce41c3e27703a2df60eacf6bffe522dbcc73326e
Manuel Luna <manuellunada@gmail.com>
2025-08-19 15:23:03 -0600

    kitty custom tabbar

---
+++ .config/kitty/tab_bar.py
@@ -0,0 +1,67 @@
+import platform
+from kitty.fast_data_types import Screen, get_options
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    TabBarData,
+    TabAccessor,
+    as_rgb
+)
+from kitty.utils import color_as_int, log_error
+
+opts = get_options()
+
+SHELLS = {
+    "fish": True,
+    "bash": True,
+    "zsh": True,
+    "sh": True,
+}
+
+def is_default_title(name: str) -> bool:
+    name = name.lower().strip()
+    return name in SHELLS or name == "~"
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    # draw left side (tab by tab)
+    accessor = TabAccessor(tab.tab_id)
+
+    title = tab.title
+    if is_default_title(title):
+        title = platform.node()
+
+    bg = opts.inactive_tab_background
+    fg = opts.inactive_tab_foreground
+    if tab.is_active:
+        bg = opts.active_tab_background
+        fg = opts.active_tab_foreground
+
+    screen.cursor.bg = as_rgb(color_as_int(bg))
+    screen.cursor.fg = as_rgb(color_as_int(fg))
+
+    activity_indicator = ""
+    if tab.has_activity_since_last_focus or tab.needs_attention:
+        activity_indicator = "*"
+
+    screen.draw(f" {index} {title}{activity_indicator} ")
+
+    # draw right side
+    if is_last:
+        right_side = " KITTY "
+        width = screen.columns
+        screen.cursor.x = width - len(right_side)
+        screen.cursor.bg = as_rgb(color_as_int(opts.active_tab_background))
+        screen.cursor.fg = as_rgb(color_as_int(opts.active_tab_foreground))
+        screen.draw(right_side)
+
+    # idk why do we need to return this but whatever
+    return screen.cursor.x
