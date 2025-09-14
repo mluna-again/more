62a9f910a7dd9837afb496ca2e21d7d2b307547d
Manuel Luna <manuellunada@gmail.com>
2025-09-14 14:02:04 -0600

    tmux spotify goback logic

--- .local/scripts/bin/tmux_spotify.sh
+++ .local/scripts/bin/tmux_spotify.sh
@@ -25,13 +25,26 @@ if ! command -v spotify_player &>/dev/null; then
 fi
 
 # use a window named "music" instead of a popup, to reduce startup time
+original_win=$(tmux display -p '#{window_id}')
 while read -r win_id win_name; do
   [ "$win_name" != music ] && continue
 
   tmux select-window -t "$win_id" || exit
   read -r pane_id pane_cmd < <(tmux list-panes -t "$win_id" -F '#{pane_id} #{pane_current_command}' | head -1) || exit
 
-  looks_empty "$pane_cmd" || exit 0
+  if [ "$original_win" = "$win_id" ] && [ "$pane_cmd" = spotify_player ]; then
+    tmux select-window -l
+    exit 0
+  fi
+
+  if ! looks_empty "$pane_cmd"; then
+    if [ "$pane_cmd" != spotify_player ]; then
+      tmux display "Other program is running in current window. Or multiple panes open."
+      exit 0
+    fi
+
+    exit 0
+  fi
 
   tmux send-keys -t "$pane_id" spotify_player Enter || exit
 
