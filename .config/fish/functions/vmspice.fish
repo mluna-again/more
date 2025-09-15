3a0929f469b31ce98507a82fe1d3c1c44bf378a9
Manuel Luna <manuellunada@gmail.com>
2025-09-15 13:35:44 -0600

    fish vmspice func

---
+++ .config/fish/functions/vmspice.fish
@@ -0,0 +1,23 @@
+function vmspice
+  if echo $argv | grep -iq help
+    printf "Usage:\n\$ vmspice\n"
+    return 1
+  end
+
+  if not test -d ~/VMs
+    echo "No VMs directory." 1>&2
+    return 1
+  end
+
+  set -l config_files (find ~/VMs -type f -iname "*.conf" -exec basename {} \;)
+  if test -z "$config_files"
+    echo "No config files detected." 1>&2
+    return 1
+  end
+  set -l selected (echo "$config_files" | fzf)
+  if test -z "$selected"
+    return 1
+  end
+
+  quickemu --vm ~/VMs/$selected --display spice
+end
