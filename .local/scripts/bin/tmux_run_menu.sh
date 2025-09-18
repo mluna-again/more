9bdd2b72573a1c8d8d322ecc976466b74b7bea72
Manuel Luna <manuellunada@gmail.com>
2025-09-18 13:11:44 -0600

    tmux add reload config cmd

--- .local/scripts/bin/tmux_run_menu.sh
+++ .local/scripts/bin/tmux_run_menu.sh
@@ -11,6 +11,7 @@ options="${options} 'Matrix' M 'run-shell ~/.local/scripts/bin/tmux_matrix.sh'"
 options="${options} 'Close empty panels' X 'run-shell ~/.local/scripts/bin/tmux_close_empty_panels.sh'"
 options="${options} 'Toggle panel borders' P 'run-shell ~/.local/scripts/bin/tmux_toggle_panel_borders.sh'"
 options="${options} 'Private mode' p 'run-shell ~/.local/scripts/bin/tmux_priv.sh'"
+options="${options} 'Reload TMUX config' R 'run-shell ~/.local/scripts/bin/tmux_reload.sh'"
 options="${options} 'Autism' $ 'run-shell ~/.local/scripts/bin/tmux_hacktheworld.sh'"
 
 eval tmux display-menu -T " Runner " -x C -y "10%" "$options"
