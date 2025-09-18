55bd94cd2c8cc58c1d4caba725a92520638fe368
Manuel Luna <manuellunada@gmail.com>
2025-09-18 13:14:40 -0600

    chafa format

--- .local/scripts/bin/amogus.sh
+++ .local/scripts/bin/amogus.sh
@@ -5,4 +5,4 @@ path="$HOME/.local/non_ascii/amogus.gif"
 clear
 # this one needs a little bit to catch up with the window size changing
 sleep 1
-chafa "$path" --center true --animate off --colors none | nms -a && chafa "$path" --center true
+chafa "$path" --format symbols --center true --animate off --colors none | nms -a && chafa --format symbols "$path" --center true
