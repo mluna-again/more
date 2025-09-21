3cf5116bdbd8c6926335056aaeeafa76428d7216
Manuel Luna <manuellunada@gmail.com>
2025-09-20 20:02:22 -0600

    tmux_spotify.sh use session instead of window

--- .local/scripts/bin/tmux_spotify.sh
+++ .local/scripts/bin/tmux_spotify.sh
@@ -2,84 +2,43 @@
 
 source ~/.local/scripts/bin/tmux_util.sh || exit
 
-# SPOTIFY (BOP) INSTRUCTIONS
-# Clone shinobu and follow the instructions below:
-# https://github.com/mluna-again/shinobu/tree/master/.local/scripts/bop
-
-# bop is tired :(
-# id=$(docker container ls | awk '$0 ~ "bop-server" { print $1 }')
-# if [ -z "$id" ]; then
-#   tmux display-message -d 0 "could not find bop, is it running? (check tmux_spotify.sh for details)"
-#   exit 0
-# fi
-#
-# options="'Player' m 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui player'"
-# options="${options} 'Selector' s 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui select'"
-# options="${options} 'Queue' q 'display-popup -w 95% -h 95% -y S -EE docker container exec -it $id bop tui queue'"
-# eval tmux display-menu -T " Runner " -x C -y "10%" "$options"
+_TSESSION=music
+_TMUSICPLAYER=spotify_player
 
 # cargo install spotify_player --features image,fzf
-if ! command -v spotify_player &>/dev/null; then
-  tmux display-message "spotify_player is not installed. Remember to run spotify_player authenticate afterwards."
+if ! command -v "$_TMUSICPLAYER" &>/dev/null; then
+  tmux display-message "$_TMUSICPLAYER is not installed. Remember to run $_TMUSICPLAYER authenticate afterwards."
   exit 0
 fi
 
-can_open_instance() {
-  instance_count=$(tmux list-panes -a -F '#{pane_current_command}' | grep -i --count spotify_player)
+current_session=$(tmux display -p '#{session_name}') || exit
 
-  if (( instance_count > 0 )); then
-    return 1
-  fi
-
-  return 0
-}
-
-dup_instance_err() {
-  session=$(tmux list-panes -a -F '#{session_name} #{pane_current_command}' | awk '$2 ~ /spotify_player/ {print $1}' | head -1) || exit
-  tmux display "You have another spotify_player instance running already in session: $session."
+if ! tmux switch-client -t "$_TSESSION"; then
+  tmux new-session -d -c ~ -n "$_TSESSION" -s "$_TMUSICPLAYER" "$_TMUSICPLAYER"
+  tmux switch-client -t todo
   exit 0
-}
-
-# use a window named "music" instead of a popup, to reduce startup time
-original_win=$(tmux display -p '#{window_id}')
-while read -r win_id win_name; do
-  [ "$win_name" != music ] && continue
-
-  tmux select-window -t "$win_id" || exit
-  read -r pane_id pane_cmd < <(tmux list-panes -t "$win_id" -F '#{pane_id} #{pane_current_command}' | head -1) || exit
-
-  if [ "$original_win" = "$win_id" ] && [ "$pane_cmd" = spotify_player ]; then
-    tmux select-window -l
-    exit 0
-  fi
-
-  pane_count=$(tmux list-panes | wc -l) || exit
-  if (( pane_count > 1 )); then
-    tmux display "More than one pane open."
-    exit 0
-  fi
-
-  if ! looks_empty "$pane_cmd"; then
-    if [ "$pane_cmd" != spotify_player ]; then
-      tmux display "Another program running."
-      exit 0
-    fi
-
-    exit 0
-  fi
-
-  if ! can_open_instance; then
-    dup_instance_err
-  fi
-  tmux send-keys -t "$pane_id" spotify_player Enter || exit
-
-  exit 0
-done < <(tmux list-windows -F '#{window_id} #{window_name}')
-
-
-if ! can_open_instance; then
-  dup_instance_err
 fi
-tmux display-popup -w 95% -h 95% -y S -EE spotify_player
 
-true
+read -r pane cmd < <(tmux display -p '#{pane_id} #{pane_current_command}' | head -1) || exit
+
+if [ "$current_session" = "$_TSESSION" ] && [ "$cmd" = "$_TMUSICPLAYER" ]; then
+  tmux switch-client -l
+  exit 0
+fi
+
+pane_count=$(tmux list-panes -t todo | wc -l)
+if (( pane_count > 1 )); then
+  tmux display "More than one pane open."
+  exit 0
+fi
+
+if ! looks_empty "$cmd"; then
+  if [ "$cmd" != "$_TMUSICPLAYER" ]; then
+    tmux display "Another program running."
+    exit 0
+  fi
+
+  exit 0
+fi
+
+tmux send-keys -t "$pane" "$_TMUSICPLAYER" Enter
