bff98ebe548cade762555cc72a6bb29dc6921fe2
Manuel Luna <manuellunada@gmail.com>
2025-09-21 11:11:33 -0600

    nvim oil show path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -423,6 +423,16 @@ require("lazy").setup({
       "stevearc/oil.nvim",
       config = function()
         local oil = require("oil")
+        function _G.get_oil_winbar()
+          local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+          local dir = require("oil").get_current_dir(bufnr)
+          if dir then
+            return vim.fn.fnamemodify(dir, ":~")
+          else
+            -- If there is no current directory (e.g. over ssh), just show the buffer name
+            return vim.api.nvim_buf_get_name(0)
+          end
+        end
 
         require("oil").setup({
           columns = {
@@ -491,6 +501,7 @@ require("lazy").setup({
             },
           },
           win_options = {
+            winbar = "%!v:lua.get_oil_winbar()",
             -- winhighlight = "Normal:OilBackground,FloatBorder:OilBorder,FloatTitle:OilBorder,LineNr:OilBackground",
           },
           skip_confirm_for_simple_edits = true,
