d85a8e66d73d601cde628d46ab0ab43145b2a100
Manuel Luna <manuellunada@gmail.com>
2025-09-22 22:56:54 -0600

    nvim icons are for the weak

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -9,7 +9,7 @@ return {
       sections = {
         { section = "header", gap = 0, padding = 0 },
         { section = "keys", gap = 0, padding = 0 },
-        { section = "startup" },
+        { section = "startup", icon = "" },
       },
       preset = {
         header = [[
@@ -33,14 +33,14 @@ return {
 ⠀▌                                                     ▐  
 ⠀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ]],
         keys = {
-          { icon = " ", key = "<leader>ff", desc = "Find Files", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "<leader>cn", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "<leader>fw", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "<leader>fo", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "<leader>co", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "<leader>sl", desc = "Restore Session", action = "<cmd>SessionLoad<cr>" },
-          { icon = "󰒲 ", key = "<leader>L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          { icon = "", key = "<leader>ff", desc = "Find Files", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "", key = "<leader>cn", desc = "New File", action = ":ene | startinsert" },
+          { icon = "", key = "<leader>fw", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "", key = "<leader>fo", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = "", key = "<leader>co", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = "", key = "<leader>sl", desc = "Restore Session", action = "<cmd>SessionLoad<cr>" },
+          { icon = "", key = "<leader>L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = "", key = "q", desc = "Quit", action = ":qa" },
         },
       },
     },
