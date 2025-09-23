77898f7f728e917aa621ae6c87dbd89d16d11f26
Manuel Luna <manuellunada@gmail.com>
2025-09-22 20:47:15 -0600

    nvim enable indent with treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -28,6 +28,6 @@ return {
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       -- additional_vim_regex_highlighting = { "ruby" },
     },
-    -- indent = { enable = true, disable = { "ruby" } },
+    indent = { enable = true, disable = { "ruby" } },
   },
 }
