e7d70308034550a829a16878d063f7d38583c19c
Manuel Luna <manuellunada@gmail.com>
2025-09-26 18:50:42 -0600

    tmux_new_session.sh fix status code

--- .local/scripts/bin/tmux_new_session.sh
+++ .local/scripts/bin/tmux_new_session.sh
@@ -3,7 +3,9 @@
 source ~/.local/scripts/bin/tmux_util.sh || exit
 
 name=$(tmux_ask "Session name:") || exit
-[ -z "$name" ] && exit 1
+[ -z "$name" ] && exit 0
 
 tmux new-session -d -c ~ -s "$name"
-tmux switch-client -t "$name"
+tmux switch-client -t "$name" || exit
+
+true
