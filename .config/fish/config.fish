18d8f0ade2404359822712061a5c0a2db0f249e4
Manuel Luna <manuellunada@gmail.com>
2025-09-26 21:52:40 -0600

    fish bun path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,8 @@ set -U fish_user_paths /usr/local/bin \
     "$HOME/.local/zig" \
     "$HOME/.local/odin" \
     "$HOME/.local/bin/nvim/bin" \
-    "$HOME/.local/share/nvim/mason/bin"
+    "$HOME/.local/share/nvim/mason/bin" \
+    "$HOME/.bun/bin"
 
 bind -M insert \ce end-of-line
 bind -M insert \ca beginning-of-line
