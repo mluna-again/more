046f1d23fa7c533c5a3d49d328363c4b01e4c325
Manuel Luna <manuellunada@gmail.com>
2025-09-30 17:22:00 -0600

    fix tmux_stop_apps.sh

--- .local/scripts/bin/tmux_stop_apps.sh
+++ .local/scripts/bin/tmux_stop_apps.sh
@@ -1,16 +1,7 @@
 #! /usr/bin/env bash
 
-_SHELLS="bash fish sh zsh"
-
 while read -r pane; do
-  id="$(awk '{print $1}' <<< "$pane")"
-  shell="$(awk '{print $2}' <<< "$pane")"
-
-  if grep -qi "$shell" <<< "$_SHELLS"; then
-    continue
-  fi
-
-  tmux send-keys -t "$id" C-c
-done < <(tmux list-panes -F '#{pane_id} #{pane_current_command}')
+  tmux send-keys -t "$pane" C-c
+done < <(tmux list-panes -F '#{pane_id}')
 
 true
