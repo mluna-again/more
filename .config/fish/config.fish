602042dde20f34d92cf3f4309524f1a01d5e9dea
Manuel Luna <manuellunada@gmail.com>
2025-10-05 14:13:53 -0600

    fish fish_title

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -128,12 +128,17 @@ end
 set --erase _asdf_shims
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    command -v atuin &>/dev/null; and atuin init fish --disable-up-arrow | source
-    command -vq fortune; and fortune
-    command -vq direnv; and direnv hook fish | source
-    command -vq zoxide; and zoxide init fish | source
-    command -vq starship; and starship init fish | source
+  # Commands to run in interactive sessions can go here
+  command -v atuin &>/dev/null; and atuin init fish --disable-up-arrow | source
+  command -vq fortune; and fortune
+  command -vq direnv; and direnv hook fish | source
+  command -vq zoxide; and zoxide init fish | source
+  command -vq starship; and starship init fish | source
+
+  set -g _host (hostname)
+  function fish_title
+    echo "$_host"
+  end
 end
 
 if test -n "$fish_private_mode"
