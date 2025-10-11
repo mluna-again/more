fc8cfe80e3465735190194fd57b4c5d608c1d0fc
Manuel Luna <manuellunada@gmail.com>
2025-10-11 00:09:46 -0600

    tmux_samurai.sh switch session

--- .local/scripts/bin/tmux_samurai.sh
+++ .local/scripts/bin/tmux_samurai.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
 sessions=$(tmux list-sessions -F '#{session_last_attached}@#{session_name}' | sort -r | awk -F@ '{printf "%s@%s\n", $2, $1}') || exit
-tmux display-popup -B -s bg=terminal -h 100% -w 100% -EE "echo \"$sessions\" | samurai"
+tmux display-popup -B -s bg=terminal -h 100% -w 100% -EE sh -c "echo \"$sessions\" | samurai >~/.cache/samurai_response"
+
+response=$(cat ~/.cache/samurai_response) || exit
+[ -z "$response" ] && exit 0
+
+tmux switch-client -t "$response"
