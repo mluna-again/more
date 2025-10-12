d9fe9e8659e4e00c4772b1ab01b4d8cccd6716d9
Manuel Luna <manuellunada@gmail.com>
2025-10-12 09:42:24 -0600

    tmux_samurai.sh use horizontal layout

--- .local/scripts/bin/tmux_samurai.sh
+++ .local/scripts/bin/tmux_samurai.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 sessions=$(tmux list-sessions -F '#{session_last_attached}@#{session_name}' | sort -r | awk -F@ '{printf "%s@%s\n", $2, $1}') || exit
-tmux display-popup -B -s bg=terminal -h 100% -w 100% -EE sh -c "echo \"$sessions\" | samurai >~/.cache/samurai_response"
+tmux display-popup -B -s bg=terminal -h 100% -w 100% -EE sh -c "echo \"$sessions\" | samurai -layout horizontal >~/.cache/samurai_response"
 
 response=$(cat ~/.cache/samurai_response) || exit
 [ -z "$response" ] && exit 0
