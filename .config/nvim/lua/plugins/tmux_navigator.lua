60cefa2316f08d870339aab92a4901ddf57b354f
Manuel Luna <manuellunada@gmail.com>
2025-10-15 19:41:06 -0600

    nvim tmux_navigator

---
+++ .config/nvim/lua/plugins/tmux_navigator.lua
@@ -0,0 +1,18 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Go to left window" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Go to down window" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Go to up window" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Go to right window" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Go to previous window" },
+  },
+}
