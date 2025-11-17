912e677d7df5dfc2cb93367029f3e4b9f5947f0c
Manuel Luna <manuellunada@gmail.com>
2025-11-17 17:21:39 -0600

    nvim conjure mappings

--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -6,8 +6,10 @@ return {
   init = function()
     vim.g["conjure#mapping#enable_defaults"] = false
     vim.g["conjure#mapping#prefix"] = "<leader>c"
+    vim.g["conjure#mapping#log_toggle"] = "l"
     vim.g["conjure#mapping#eval_buf"] = "b"
-    vim.g["conjure#mapping#eval_current_form"] = "f"
+    vim.g["conjure#mapping#eval_root_form"] = "f"
+    vim.g["conjure#mapping#eval_current_form"] = "F"
     vim.g["conjure#mapping#eval_visual"] = "v"
   end
 }
