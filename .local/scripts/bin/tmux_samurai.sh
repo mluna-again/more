bfd102f3bf4960d41c701dca0a020c620031e01a
Manuel Luna <manuellunada@gmail.com>
2025-12-17 16:02:59 -0600

    tmux filter quake session

--- .local/scripts/bin/tmux_samurai.sh
+++ .local/scripts/bin/tmux_samurai.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sessions=$(tmux list-sessions -F '#{session_last_attached}@#{session_name}' | sort -hr | awk -F@ '{printf "%s@%s\n", $2, $1}') || exit
+sessions=$(tmux list-sessions -f '#{!=:#{session_name},quake}' -F '#{session_last_attached}@#{session_name}' | sort -hr | awk -F@ '{printf "%s@%s\n", $2, $1}') || exit
 tmux display-popup -B -s bg=terminal -h 100% -w 100% -EE sh -c "echo \"$sessions\" | samurai -layout horizontal >~/.cache/samurai_response"
 
 response=$(cat ~/.cache/samurai_response) || exit
--- .local/scripts/bin/tmux_switch_session.sh
+++ .local/scripts/bin/tmux_switch_session.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-output=$(tmux list-windows -a -F "#{session_id} #{window_id} #{session_name}: #{window_name}" | mina -nth 2,3 -title="Search a TMUX session" -icon="") || exit
+output=$(tmux list-windows -f '#{!=:#{session_name},quake}' -a -F "#{session_id} #{window_id} #{session_name}: #{window_name}" | mina -nth 2,3 -title="Search a TMUX session" -icon="") || exit
 
 [ -z "$output" ] && exit
 
