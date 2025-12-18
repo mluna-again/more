7c1de037a8f546a0b3f3a5a2ebc312f7d1917700
Manuel Luna <manuellunada@gmail.com>
2025-12-18 01:26:28 -0600

    tmux_delete_session.sh

---
+++ .local/scripts/bin/tmux_delete_session.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+source ~/.local/scripts/bin/tmux_util.sh || exit
+
+response=$(tmux_prompt "Are you sure?")
+if ! [[ "${response,,}" =~ y(es)? ]]; then
+  exit 0
+fi
+
+tmux kill-session -t .
