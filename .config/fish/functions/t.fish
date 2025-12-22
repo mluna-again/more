8c434bcfe48b2f0f64d2ebf28fec6bb858da7e2d
Manuel Luna <manuellunada@gmail.com>
2025-12-22 16:48:03 -0600

    tmux default session name

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -1,20 +1,14 @@
 function t
-    if test -n "$SSH_CONNECTION"
-        if pgrep -u "$USER" tmux &>/dev/null
-            tmux set -g @force_ssh_indicator true
-            tmux attach
-            return
-        end
-
-        tmux new-session \; set -g @force_ssh_indicator true
-        return
-    end
-
     if pgrep -u "$USER" tmux &>/dev/null
-        tmux attach
+      if tmux has-session -t void &>/dev/null
+        tmux attach -t void
         return
+      end
+
+      tmux new-session -A -n null -s void
+      return
     end
 
-    tmux
+    tmux new-session -A -n null -s void
 end
 
