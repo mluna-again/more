42df16fa222b6f9b1b533b7f2332fc589aedcd2d
Manuel Luna <manuellunada@gmail.com>
2026-01-03 21:21:47 -0600

    tmux fix online script on osx

--- .local/scripts/bin/tmux_online.sh
+++ .local/scripts/bin/tmux_online.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-if ! ping -w 1 -c 1 8.8.8.8 &>/dev/null && ! ping -w 1 -c 1 1.1.1.1 &>/dev/null; then
+if ! ping -W 1 -c 1 8.8.8.8 &>/dev/null && ! ping -W 1 -c 1 1.1.1.1 &>/dev/null; then
   echo " OFF "
 fi
