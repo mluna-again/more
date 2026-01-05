f41facfbf78f61f7307f92d3970494747d2ef119
Manuel Luna <manuellunada@gmail.com>
2026-01-04 19:55:37 -0600

    tmux mini-server.sh status bar on top

--- .config/tmux/mini-server.sh
+++ .config/tmux/mini-server.sh
@@ -3,24 +3,6 @@
 # same as mini.sh but "tailored" for servers.
 # and by tailored i mean it just displays uptime in the status bar
 
-sed -e 's|\(.*plugins/tpm/tpm.*\)|# \1|g' \
-  -e 's|run-shell .*rename_session.sh.*$|command-prompt -p "Rename session:" "rename-session -t . '%%'"|g' \
-  -e 's|run-shell .*search_backwards.sh.*$|command-prompt -T search -p "(search up)" { send-keys -X search-backward "%%" }|g ' \
-  -e 's|run-shell .*search_forwards.sh.*$|command-prompt -T search -p "(search down)" { send-keys -X search-forward "%%" }|g ' \
-  -e 's|run-shell .*rename_window.*$|command-prompt -p "Rename window:" "rename-window -t . %%"|g ' \
-  -e 's|^bind.*run-shell .*resize.sh.*$|bind Left resize-pane -t . -L 5\nbind Right resize-pane -t . -R 5\nbind Up resize-pane -t . -U 5\nbind Down resize-pane -t . -D 5|g ' \
-  -e 's|run-shell .*reload.sh.*$|source-file ~/.tmux.conf \\; display-message "Config reloaded"|g ' \
-  -e 's|display-popup .*switch_session.sh.*$|choose-tree|g ' \
-  -e 's|status-right "\(.*\)"|status-right "#(uptime --pretty) \1"|g' \
-  -e "s|status-right '\(.*\)'|status-right \"#(uptime --pretty) \1\"|g" \
-  -e "s|status-position.*|status-position bottom|g" \
-  ~/.tmux.conf | \
-  awk '{
-    if ($0 ~ /run-shell/) {
-      msg = sprintf("display-message \"%s not implemented\"\n", $4);
-      gsub(/run-shell .*/, msg, $0);
-      printf $0;
-    } else {
-      print $0;
-    }
-  }'
+~/.config/tmux/mini.sh | \
+  sed -e 's|status-right "\(.*\)"|status-right "#(uptime --pretty) \1"|g' \
+  -e "s|status-right '\(.*\)'|status-right \"#(uptime --pretty) \1\"|g"
