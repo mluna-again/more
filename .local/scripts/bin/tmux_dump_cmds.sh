8f8888390320492f319381622887b9ec1dcc3f82
Manuel Luna <manuellunada@gmail.com>
2026-02-23 15:18:52 -0600

    tmux_dump_cmds.sh output format

--- .local/scripts/bin/tmux_dump_cmds.sh
+++ .local/scripts/bin/tmux_dump_cmds.sh
@@ -5,11 +5,12 @@ source ~/.local/scripts/bin/tmux_util.sh || exit
 # Dumps current window's panes commands.
 # This only works if your shell is correctly setting the title with the running command + args.
 # This script assumes the following format: [<date>] <cmd> <args> @ <path>
+# Outputs the following: <path>@<cmd> <args>
 
 cmds=$(
   tmux list-panes -F '#{pane_title}' | \
     sed 's|\[.*\]||' | \
-    awk -F@ '{ gsub(/^ */, "", $1); gsub(/ *$/, "", $1); print $1 }'
+    awk -F@ '{ gsub(/^ */, "", $1); gsub(/ *$/, "", $1); printf "%s@%s\n", $2, $1 }'
 )
 
 if [ -z "$cmds" ]; then
@@ -17,13 +18,14 @@ if [ -z "$cmds" ]; then
   exit 0
 fi
 
-path=$(tmux display -p '#{pane_current_path}/cmds.xdo')
-path=$(tmux_ask 'Destination' "$path")
+path=$(tmux display -p '#{pane_current_path}')
+path=$(tmux_ask 'Destination dir' "$path")
 [ -z "$path" ] && exit
+path="$path/cmds.xdo"
 
 if [ -f "$path" ]; then
-  tmux_alert "File already exists"
-  exit 0
+  tmux_alert "File $(basename "$path") already exists (appending)."
+  echo "----" >> "$path"
 fi
 
 if ! echo "$cmds" >> "$path"; then
