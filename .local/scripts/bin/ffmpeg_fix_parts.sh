2f86131993b967c3f84cffc44c857fd0eb96d674
Manuel Luna <manuellunada@gmail.com>
2026-02-28 14:11:18 -0600

    ffmpeg_fix_parts.sh force flag

--- .local/scripts/bin/ffmpeg_fix_parts.sh
+++ .local/scripts/bin/ffmpeg_fix_parts.sh
@@ -11,7 +11,8 @@ Options
   -h, --help                   show this message
   -o FORMAT, --format FORMAT   specify output format (default: mp4)
   -f FILTER, --filter FILTER   find's filter (default: '*.part')
-  -d DEPTH, --filter DEPTH     find's depth (default: 10)
+  -d DEPTH, --filter DEPTH     find's depth (default: 2)
+  --force                      remux everything (default: false)
 EOF
   exit 1
 }
@@ -21,7 +22,8 @@ EOF
 format="mp4"
 dir=
 filter="*.part"
-depth="10"
+depth="2"
+force=0
 while true; do
   [ -z "$1" ] && break
 
@@ -45,6 +47,10 @@ while true; do
       depth="$1"
       ;;
 
+    --force)
+      force=1
+      ;;
+
     *)
       dir="$1"
       ;;
@@ -60,13 +66,13 @@ old_files=()
 files_already_fixed=()
 while read -r file; do
   fixed_file="$(sed "s|\(.*\)\..*\.part|\1\.${format}|" <<< "$file")" || exit
-  if [ -f "$fixed_file" ]; then
+  if [ -f "$fixed_file" ] && [ "$force" -ne 1 ]; then
     echo "$fixed_file already exists." >&2
     files_already_fixed+=("$file")
     continue
   fi
 
-  cmd="ffmpeg -i \"$file\" -c copy \"$fixed_file\""
+  cmd="ffmpeg -i \"$file\" -c copy -y \"$fixed_file\""
   commands+=("$cmd")
   old_files+=("$file")
   echo "$cmd"
@@ -84,18 +90,21 @@ if [[ ! "${response,,}" =~ ^y(es)?$ ]]; then
 fi
 
 files_with_errs=()
-files_to_delete=()
 for cmd in "${commands[@]}"; do
   if ! eval "$cmd"; then
     files_with_errs+=("$cmd")
     continue
   fi
-  files_to_delete+=("$cmd")
 done
 
-if [[ "${#files_to_delete[@]}" -gt 0 ]]; then
-  for file in "${files_to_delete[@]}"; do
+if [[ "${#old_files[@]}" -gt 0 ]]; then
+  files_to_delete=()
+  for file in "${old_files[@]}"; do
+    if echo "$files_with_errs" | grep "$file"; then
+      continue
+    fi
     echo rm "$file"
+    files_to_delete+=("$file")
   done
 
   printf "\nRemove .part files? [N/y] "
