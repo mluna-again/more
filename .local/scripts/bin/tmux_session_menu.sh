e3e194726a76dc9b2cdda976fb33ecd156d79d4c
Manuel Luna <manuellunada@gmail.com>
2026-03-04 23:02:28 -0600

    tmux_session_menu.sh check client size

--- .local/scripts/bin/tmux_session_menu.sh
+++ .local/scripts/bin/tmux_session_menu.sh
@@ -2,6 +2,13 @@
 
 source ~/.local/scripts/bin/tmux_util.sh || exit
 
+session_count=$(tmux list-sessions | wc -l)
+client_height=$(tmux display -p '#{client_height}')
+if (( session_count >= ( client_height - 3 ) )); then
+  tmux_alert "Not enough space!"
+  exit 0
+fi
+
 session_names=$(tmux list-sessions -F '#{session_name}' | sort --unique | awk '{printf "\"%-20s\" \"\" \"switch-client -t %s\" ", $1, $1}')
 [ -z "$session_names" ] && return
 
