a515b378ae3f0e25dcdff38ced30e98a47b49297
Manuel Luna <manuellunada@gmail.com>
2026-04-01 02:48:04 -0600

    tmux menu zoom opt

--- .local/scripts/bin/tmux_tmux_menu.sh
+++ .local/scripts/bin/tmux_tmux_menu.sh
@@ -2,4 +2,5 @@
 
 tmux display-menu -x 0 -y S -M -O \
   "Change prefix" "" "run-shell ~/.local/scripts/bin/tmux_toggle_prefix.sh" \
+  "Toggle zoom" "" "resize-pane -Z" \
   "Detach" "" "detach-client"
