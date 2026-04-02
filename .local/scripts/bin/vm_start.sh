76199b0ab80844a33bb3b957e706e9e6cfc90433
Manuel Luna <manuellunada@gmail.com>
2026-04-01 18:08:51 -0600

    vm_start.sh --gui flag

--- .local/scripts/bin/vm_start.sh
+++ .local/scripts/bin/vm_start.sh
@@ -4,20 +4,29 @@ usage() {
   cat - <<EOF
 Just a convenience script.
 Start a quickemu VM.
+It starts it in headless mode by default.
 
 Usage:
 $ vm_start.sh <.conf file>
+
+Flags:
+  --gui   opens a gtk gui.
 EOF
   exit 1
 }
 
 vm=
+gui=0
 while true; do
   [ -z "$1" ] && break
 
   case "$1" in
     --help|-h|help)
       usage
+      ;;
+
+    --gui)
+      gui=1
       ;;
 
     *)
@@ -30,7 +39,14 @@ done
 
 [ -z "$vm" ] && usage
 
-quickemu --vm "$vm"
+opts=()
+if [ "$gui" -eq 1 ]; then
+  opts+=( --display gtk )
+else
+  opts+=( --display none )
+fi
+
+quickemu --vm "$vm" "${opts[@]}"
 
 dir="${vm/.conf/}"
 echo
