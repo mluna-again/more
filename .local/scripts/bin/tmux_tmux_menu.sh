0faf242a6760f8484a6bb72a1d2ade51c76554c6
Manuel Luna <manuellunada@gmail.com>
2026-04-02 13:42:32 -0600

    tmux_tmux_menu.sh more options

--- .local/scripts/bin/tmux_tmux_menu.sh
+++ .local/scripts/bin/tmux_tmux_menu.sh
@@ -3,4 +3,6 @@
 tmux display-menu -x 0 -y S -M -O \
   "Change prefix" "" "run-shell ~/.local/scripts/bin/tmux_toggle_prefix.sh" \
   "Toggle zoom" "" "resize-pane -Z" \
+  "Toggle borders" "" "run-shell ~/.local/scripts/bin/tmux_toggle_panel_borders.sh" \
+  "-------" "" "" \
   "Detach" "" "detach-client"
