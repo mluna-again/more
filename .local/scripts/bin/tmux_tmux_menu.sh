c346dc4ea5f6c61d5082d89362553fcb2deecdd7
Manuel Luna <manuellunada@gmail.com>
2026-04-04 23:30:37 -0600

    tmux_tmux_menu.sh last session opt

--- .local/scripts/bin/tmux_tmux_menu.sh
+++ .local/scripts/bin/tmux_tmux_menu.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env bash
 
-tmux display-menu -x 0 -y S -M -O \
+lsess=$(tmux display -p '#{client_last_session}')
+lsess_pref=""
+[ -z "$lsess" ] && lsess_pref="-"
+
+tmux display-menu -x 0 -y S -M -O -- \
+  "${lsess_pref}Last session ($lsess)" "" "switch-client -l" \
+  "-------" "" "" \
   "Change prefix" "" "run-shell ~/.local/scripts/bin/tmux_toggle_prefix.sh" \
   "Toggle zoom" "" "resize-pane -Z" \
   "Toggle borders" "" "run-shell ~/.local/scripts/bin/tmux_toggle_panel_borders.sh" \
