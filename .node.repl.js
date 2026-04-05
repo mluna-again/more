3ef654776cac684f8e9bfd85c15411aa0fd48e72
Manuel Luna <manuellunada@gmail.com>
2026-04-05 17:54:34 -0600

    node repl print version

--- .node.repl.js
+++ .node.repl.js
@@ -1,3 +1,10 @@
 const { styleText } = require("node:util");
-const repl = require("repl");
+const repl = require("node:repl");
+
+let v8version = "";
+if (process.versions.v8) {
+  v8version = ` (v8 ${process.versions.v8})`
+}
+console.log(`Node.js ${process.version}${v8version}`)
+
 const replServer = repl.start({ prompt: styleText("yellow", "node  ") });
