0db2def37717a8dcb5e5a912cf37270529b4a3c2
Manuel Luna <manuellunada@gmail.com>
2026-04-13 18:30:17 -0600

    tmux_layout_selector.sh custom centered layout

--- .local/scripts/bin/tmux_layout_selector.sh
+++ .local/scripts/bin/tmux_layout_selector.sh
@@ -8,10 +8,20 @@ Main Horizontal Mirrored
 Main Vertical
 Main Vertical Mirrored
 Tiled
+Centered
 EOF
 )
 
 [ -z "$layout" ] && exit 0
+
+case "$layout" in
+  "Centered")
+    tmux select-layout "f4d4,205x32,0,0{57x32,0,0[57x16,0,0,55,57x15,0,17,62],87x32,58,0,60,59x32,146,0[59x16,146,0,61,59x15,146,17,63]}" && \
+      tmux swap-pane -s . -t 2 && \
+      tmux select-pane -t 2
+    exit
+    ;;
+esac
 
 layout="$(sed 's/ /-/g' <<< "$layout" | tr '[:upper:]' '[:lower:]')"
 
