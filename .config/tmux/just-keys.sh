48b9486a1c9cb4f80092a8cc01aab2de9b9e1129
Manuel Luna <manuellunada@gmail.com>
2026-04-16 16:35:17 -0600

    tmux just-keys.sh include prefix

--- .config/tmux/just-keys.sh
+++ .config/tmux/just-keys.sh
@@ -3,7 +3,8 @@
 # This prints a copy of ~/.tmux.conf
 # to STDOUT but only the config related to keybinds.
 # Removes non-native functionality (just as in mini.sh)
+# Note: also includes prefix config.
 
 ~/.config/tmux/mini.sh | \
-  grep -E -e '^bind' -e '^unbind' | \
+  grep -E -e '^bind' -e '^unbind' -e '^set .* prefix2? .*$' | \
   grep -v '.* not implemented'
