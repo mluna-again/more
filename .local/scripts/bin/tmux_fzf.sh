53a82be2bf2f69d11d863d577cf862b7966acd64
Manuel Luna <manuellunada@gmail.com>
2026-04-19 18:47:02 -0600

    tmux_fzf.sh

---
+++ .local/scripts/bin/tmux_fzf.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+die() {
+  echo "$*" 1>&2
+  exit 1
+}
+
+if ! command -v fzf &>/dev/null; then
+  die no fzf installed
+fi
+
+if [ -z "$TMUX" ]; then
+  die running outside of tmux
+fi
+
+current=$(tmux display -p '#{window_id}') || exit
+chosen=$(
+  tmux list-windows -a -F '#{window_id} #{session_name}: #{window_name}' | \
+    grep -vE "^$current" | \
+    fzf --with-nth=2.. --preview="tmux capture-pane -p -e -t {1}" | \
+    awk '{print $1}'
+)
+[ -z "$chosen" ] && exit
+
+tmux switch-client -t "$chosen"
