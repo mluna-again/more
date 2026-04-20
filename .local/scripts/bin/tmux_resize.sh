7d5eea488b7852d66e8a2cd6725a42fe052b2554
Manuel Luna <manuellunada@gmail.com>
2026-04-19 19:06:27 -0600

    tmux_resize.sh check pane count

--- .local/scripts/bin/tmux_resize.sh
+++ .local/scripts/bin/tmux_resize.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env bash
 
+source ~/.local/scripts/bin/tmux_util.sh || exit
+
 _STEP=5
+
+count=$(tmux display -p '#{window_panes}') || exit
+if (( count < 2 )); then
+  tmux_alert "Only one pane open."
+  exit 0
+fi
 
 while true; do
   key=$(tmux command-prompt -1 -p "Resize (hjkl). C-c/q to quit." 'display -p %%')
