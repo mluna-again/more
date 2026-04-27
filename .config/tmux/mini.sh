566d1b6555a32f35f32c466d00a2ca024796c7b4
Manuel Luna <manuellunada@gmail.com>
2026-04-27 14:52:57 -0600

    tmux mini.sh new-session handle spaces

--- .config/tmux/mini.sh
+++ .config/tmux/mini.sh
@@ -8,7 +8,7 @@ sed -e 's|\(.*plugins/tpm/tpm.*\)|# \1|g' \
   -e 's|run-shell .*search_backwards.sh.*$|command-prompt -T search -p "(search up)" { send-keys -X search-backward "%%" }|g ' \
   -e 's|run-shell .*search_forwards.sh.*$|command-prompt -T search -p "(search down)" { send-keys -X search-forward "%%" }|g ' \
   -e 's|run-shell .*rename_window.*$|command-prompt -p "Rename window:" "rename-window -t . %%"|g ' \
-  -e 's|run-shell .*session_new.sh.*$|command-prompt -p "New session:" "new-session -c ~ -s %%"|g ' \
+  -e 's|run-shell .*session_new.sh.*$|command-prompt -p "New session:" "new-session -c ~ -s '\''%%'\''"|g ' \
   -e 's|^bind.*run-shell .*resize.sh.*$|bind Left resize-pane -t . -L 5\nbind Right resize-pane -t . -R 5\nbind Up resize-pane -t . -U 5\nbind Down resize-pane -t . -D 5|g ' \
   -e 's|run-shell .*reload.sh.*$|source-file ~/.tmux.conf \\; display-message "Config reloaded"|g ' \
   -e 's|display-popup .*switch_session.sh.*$|choose-tree|g ' \
