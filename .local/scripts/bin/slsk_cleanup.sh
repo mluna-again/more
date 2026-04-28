3378c5f6456c9bae657b77bb75b1c044f7fec6fd
Manuel Luna <manuellunada@gmail.com>
2026-04-27 23:57:38 -0600

    slsk_cleanup.sh v2

--- .local/scripts/bin/slsk_cleanup.sh
+++ .local/scripts/bin/slsk_cleanup.sh
@@ -1,20 +1,54 @@
 #! /usr/bin/env bash
 
-usage() {
-  cat - <<EOF
-Usage:
-$ slsk_cleanup.sh [<path to album>]
-
-Cleans up album.
-  - checks missing metadata
-  - checks naming conventions
-  - adds replaygain values
-  - embeds cover (if present)
-EOF
+die() {
+  echo "$*" 1>&2
   exit 1
 }
 
-album="$PWD"
+stderr() {
+  echo "$*" 1>&2
+}
+
+if ! command -v metaflac &>/dev/null; then
+  die metaflac required
+fi
+
+# shellcheck disable=SC2120
+usage() {
+  cat - <<EOF
+Takes an album (FLAC) and makes sure it has at least the following metadata:
+  - ARTIST
+  - ALBUMARTIST
+  - TRACK/TRACKNUMBER
+  - TITLE
+  - ALBUM
+
+Also, adds replaygain data, a cover, and formats tracks titles.
+
+Other stuff:
+  - It removes 'SUBTITLE' tags
+  - It sets ALBUMARTIST if its empty and if all the tracks have the same ARTIST
+
+Usage:
+$ slsk_cleanup.sh [<album path>]
+$ slsk_cleanup.sh # uses \$PWD
+
+Flags:
+  -i|--interactive   run interactive mode to fill information if metadata is missing (only useful for fields which are the same for every song: ALBUMARTIST, ALBUM).
+EOF
+  if [ "$#" -gt 0 ]; then
+    echo
+    tput setab 1
+    tput setaf 0
+    printf " ERROR "
+    tput sgr0
+    echo " $*"
+  fi
+  exit 1
+}
+
+dir="."
+interactive=0
 while true; do
   [ -z "$1" ] && break
 
@@ -23,78 +57,174 @@ while true; do
       usage
       ;;
 
+    -i|--interactive)
+      interactive=1
+      ;;
+
     *)
-      album="$1"
+      dir="$1"
       ;;
   esac
 
   shift
 done
 
-[ -z "$album" ] && usage
-if [ ! -d "$album" ]; then
-  echo "$album is not a directory / is not accessible" >&2
-  exit 1
-fi
-
-something_wrong=0
-album_regex='^.+ - .+$'
-if [[ ! "$album" =~ $album_regex ]]; then
-  echo "album doesn't match naming convention: $album" >&2
-  something_wrong=1
-fi
-
-while read -r file; do
-  album_name="$(metaflac --show-tag=album "$file")" || break
-  album_artist="$(metaflac --show-tag=albumartist "$file")" || break
-  if [ -z "$album_artist" ]; then
-    album_artist="$(metaflac --show-tag=album_artist "$file")" || break
+validate_flacs() {
+  if [ -z "$(find "$dir" -type f -iname "*.flac")" ]; then
+    return 1
   fi
-  title="$(metaflac --show-tag=title "$file")" || break
+}
 
-  if [ -z "$album_name" ]; then
-    echo "missing album title: $file" >&2
-    something_wrong=1
+# check if track ($1) has tag ($2)
+check_tag() {
+  local track="$1" tag="$2"
+  [ -n "$(metaflac --show-tag="$tag" "$track")" ]
+}
+
+set_tag() {
+  local track="$1" tag="$2" value="$3" append="${4:-0}"
+  if [ "$append" -ne 1 ]; then
+    remove_tag "$track" "$tag" || return
   fi
 
-  if [ -z "$album_artist" ]; then
-    echo "missing album artist: $file" >&2
-    something_wrong=1
+  metaflac --set-tag="${tag}=${value}" "$track"
+}
+
+set_tag_all() {
+  local tag="$1" value="$2" append="${4:-0}"
+  if [ "$append" -ne 1 ]; then
+    metaflac --remove-tag="$tag" ./*.flac || return
   fi
 
-  if [ -z "$title" ]; then
-    echo "missing track title: $file" >&2
-    something_wrong=1
-  fi
+  metaflac --set-tag="${tag}=${value}" ./*.flac
+}
 
-  title_regex='^[0-9]+ [0-9A-Za-z].+\.flac$'
-  file_title="$(basename "$file")"
-  if [[ ! "$file_title" =~ $title_regex ]]; then
-    echo "filename doesn't match naming convention: $file_title" >&2
-    something_wrong=1
-  fi
+remove_tag() {
+  local track="$1" tag="$2"
+  metaflac --remove-tag="$tag" "$track"
+}
 
-  cover="$(ffprobe "$file" 2>&1 | grep 'Cover (front)')"
-  if [ -z "$cover" ]; then
-    if [ -f "${album}/cover.jpg" ]; then
-      metaflac --import-picture-from="${album}/cover.jpg" "$file" || exit
-      echo "cover added: $file"
-    elif [ -f "${album}/cover.png" ]; then
-      metaflac --import-picture-from="${album}/cover.png" "$file" || exit
-      echo "cover added: $file"
+remove_tag_all() {
+  local tag="$1"
+  printf "Removing SUBTITLE tag... "
+  metaflac --remove-tag="$tag" ./*.flac
+  echo "Done."
+}
+
+add_replaygain() {
+  printf "Adding replagain data... "
+  metaflac --add-replay-gain ./*.flac
+  echo "Done."
+}
+
+add_cover() {
+  local url
+  printf "Cover URL: "
+  read -r url
+  if [ -z "$url" ]; then
+    stderr URL required
+    return 1
+  fi
+  printf "Downloading cover... "
+  curl -sS "$url" -o cover.jpg || return 1
+  echo "Done."
+
+  printf "Setting cover... "
+  metaflac --remove --block-type=PICTURE --dont-use-padding ./*.flac || return 1
+  metaflac --import-picture-from=cover.jpg ./*.flac || return 1
+  echo "Done."
+}
+
+list_tracks() {
+  find "$dir" -type f -iname "*.flac"
+}
+
+format_titles() {
+  local num title cmds cmd track new_name
+  declare -A names
+  cmds=()
+  while read -r track; do
+    num="$(metaflac --show-tag=TRACK "$track" | awk -F= '{print $2}')"
+    [ -z "$num" ] && num="$(metaflac --show-tag=TRACKNUMBER "$track" | awk -F= '{print $2}')"
+    [ -z "$num" ] && die "[format_titles] $track has no number, but it should, something is wrong"
+    num="$(printf '%02d' "$num")"
+
+    title="$(metaflac --show-tag=TITLE "$track" | awk -F= '{print $2}')"
+    [ -z "$title" ] && die "[format_titles] $track has no title, but it should, something is wrong"
+
+    new_name="${num}. ${title}.flac"
+    [ "$(basename "$track")" = "$new_name" ] && continue
+
+    names["$track"]="$new_name"
+    cmds+=( "mv -i \"$track\" \"${num}. ${title}.flac\" " )
+  done < <(list_tracks)
+
+  if [ "${#cmds[@]}" -gt 0 ]; then
+    for cmd in "${cmds[@]}"; do
+      echo "$cmd"
+    done
+    printf "Continue? [N/y] "
+    read -r response
+    if [[ ! "${response,,}" =~ ^y(es)?$ ]]; then
+      return 1
+    fi
+
+    for name in "${!names[@]}"; do
+      mv -i "$name" "${names[$name]}" || return
+    done
+  fi
+}
+
+set_default_albumartist() {
+  local tracks_count albumartist_count artist artist_tag_count response
+  tracks_count=$(list_tracks | wc -l)
+  albumartist_count=$(metaflac --show-tag=ALBUMARTIST ./*.flac | wc -l)
+  if (( albumartist_count < tracks_count )); then
+    artist=$(metaflac --show-tag=ARTIST ./*.flac | awk -F= '{print $2}' | sort | uniq)
+    artist_tag_count=$(wc -l <<< "$artist")
+    if [ "$artist_tag_count" -eq 1 ]; then
+      printf "Setting ALBUMARTIST to %s, continue? [N/y] " "$artist"
+      read -r response
+      if [[ ! "${response,,}" =~ ^y(es)?$ ]]; then
+        die "Please set ALBUMARTIST first, then"
+      fi
+      set_tag_all ALBUMARTIST "$artist"
     else
-      echo "unknown/missing cover: $album" >&2
-      exit 1
+      die "Not all tracks have an ALBUMARTIST and could not infer it from ARTIST tags"
     fi
   fi
+}
 
-done < <(find "$album" -type f -iname "*.flac")
-
-if [ "$something_wrong" -ne 0 ]; then
-  exit 1
+if ! validate_flacs; then
+  die no FLACs detected
 fi
 
-metaflac --remove-replay-gain --add-replay-gain "${album}"/*.flac || exit
-echo replaygain added
+if [ "$interactive" -eq 1 ]; then
+  printf "ALBUMARTIST: "
+  read -r response
+  [ -z "$response" ] && die ALBUMARTIST required
+  set_tag_all ALBUMARTIST "$response"
 
-echo "done"
+  printf "ALBUM: "
+  read -r response
+  [ -z "$response" ] && die ALBUM required
+  set_tag_all ALBUM "$response"
+fi
+
+# Non-destructive stuff 
+while read -r track; do
+  check_tag "$track" ARTIST || die "no ARTIST in $track"
+  check_tag "$track" TITLE || die "no TITLE in $track"
+  check_tag "$track" ALBUM || die "no ALBUM in $track"
+  if ! check_tag "$track" TRACK && ! check_tag "$track" TRACKNUMBER; then
+    die "no TRACK/TRACKNUMBER in $track"
+  fi
+
+done < <(list_tracks)
+
+# Destructive stuff
+add_cover || exit
+add_replaygain || exit
+set_default_albumartist || exit
+remove_tag_all SUBTITLE || exit
+format_titles || exit
