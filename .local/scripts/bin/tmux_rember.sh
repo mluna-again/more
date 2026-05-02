f7078f8842ae62ee6b5adce156939fda59a864bd
Manuel Luna <manuellunada@gmail.com>
2026-05-02 14:38:02 -0600

    tmux_rember.sh handle small screens

--- .local/scripts/bin/tmux_rember.sh
+++ .local/scripts/bin/tmux_rember.sh
@@ -3,6 +3,14 @@
 note=$(cat ~/.cache/tmux_rember.sh 2>/dev/null)
 [ -z "$note" ] && exit 0
 
+minwidth="${1:-140}"
+tw="$(tmux display -p '#{client_width}')"
+
+if [ "$tw" -lt "$minwidth" ]; then
+  echo " N* "
+  exit 0
+fi
+
 if [ "${#note}" -gt 30 ]; then
   short_note=$(cut -c 1-30 <<< "$note")
   echo " ${short_note}... "
