da75c8bb95911c663c123fe50bed0260ec973006
Manuel Luna <manuellunada@gmail.com>
2026-05-15 23:57:35 -0600

    fish config_mini.fish

--- .config/fish/config_mini.fish
+++ .config/fish/config_mini.fish
@@ -1,38 +1,14 @@
-set -g fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_color_valid_path
 
 set -g fish_user_paths /usr/local/bin \
-    /opt/homebrew/bin \
-    /opt/homebrew/sbin \
     "$HOME/.local/go/bin" \
     "$HOME/.local/bin" \
     "$HOME/.dotnet/tools" \
     "$HOME/.cargo/bin" \
     "/usr/local/go/bin" \
     "$HOME/.local/scripts/bin" \
-    "$HOME/.local/share/bob/nvim-bin" \
-    "$HOME/.local/gem/bin" \
-    "$HOME/Scripts" \
-    "$HOME/.local/zig" \
-    "$HOME/.local/odin" \
-    "$HOME/.local/bin/spotless"
-
-function fish_title
-  set -l cmd "$argv[1]"
-  if test -z "$cmd"
-    set cmd fish
-  end
-
-  if test -z "$TMUX"
-    echo "$cmd"
-    return
-  end
-
-  set cmd (string trim "$cmd")
-
-  echo "[$(date +'%I:%M %p')] $cmd @ $PWD"
-end
+    "$HOME/.local/share/bob/nvim-bin"
 
 bind -M insert \ce end-of-line
 bind -M insert \ca beginning-of-line
@@ -54,23 +30,9 @@ bind -M insert f8 true
 bind -M insert f9 true
 bind -M insert f10 true
 
-set -gx CURL_HOME "$HOME/.config/curl"
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx COLORTERM truecolor
 set -gx VISUAL nvim
 set -gx EDITOR "$VISUAL"
-set -gx ELIXIR_ERL_OPTIONS "-kernel shell_history enabled"
-set -gx ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -gx GOPATH "$HOME/.local/go"
-set -gx SHELLCHECK_OPTS "-e SC2001 -e SC1090"
-set -gx MANPAGER "nvim -c 'set cmdheight=1' '+Man!'"
-set -gx AUTOSSH_PORT 0
-set -gx POSTING_THEME_DIRECTORY "$HOME/.config/posting/themes"
-set -gx TMUXP_CONFIGDIR "$HOME/.local/tmuxp"
-set -gx PYTHONSTARTUP "$HOME/.pythonrc"
-# Temp (probably?)
-set -gx NVIM_APPNAME nvim_again
-
 if command -vq fd
   set -gx FZF_DEFAULT_COMMAND 'fd'
 else
@@ -94,18 +56,6 @@ set -gx FZF_DEFAULT_OPTS '--ellipsis=...
   --no-scrollbar'
 
 
-abbr --add sel selune
-abbr --add pg pgcli -h 127.0.0.1 -u postgres
-abbr --add dots yadm
-abbr --add dotss yadm status
-abbr --add dotsa yadm add -u
-abbr --add dotsA yadm add
-abbr --add dotsl "yadm log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-abbr --add dotsC yadm checkout
-abbr --add dotsR yadm reset --hard
-abbr --add dotsp yadm push
-abbr --add dotsd yadm diff
-abbr --add dotsdd yadm diff --cached
 abbr --add gd git diff
 abbr --add gdd git diff --cached
 abbr --add gl "git log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
@@ -119,31 +69,7 @@ abbr --add gss git stash list -p
 abbr --add ga git add .
 abbr --add gA git add
 abbr --add gr git reset --soft
-abbr --add gofmt go fmt ./...
-abbr --add dateiso date +"%Y-%m-%dT%H:%M:%S%z"
-abbr --add emacs "emacsclient -a '' -c --tty"
-abbr --add doom '~/.config/emacs/bin/doom'
-abbr --add setup-idf source "$HOME/esp/esp-idf/export.fish"
-abbr --add esp idf.py
-abbr --add rsync rsync -avhbu --info=progress2
-abbr --add p fish -P
-abbr --add un podman unshare
-abbr --add work SHELL=/usr/bin/fish toolbox enter work
 abbr --add mv mv -i
-abbr --add rubydocs gem rdoc --all --ri --no-rdoc
-abbr --add llsblk lsblk -o "NAME,MAJ:MIN,RM,SIZE,RO,FSTYPE,MOUNTPOINT,UUID"
-abbr --add oil nvim -c 'Oil'
-abbr --add --set-cursor oilssh nvim oil-ssh://%/
-abbr --add nv NVIM_APPNAME=bare_nvim nvim
-abbr --add pager $MANPAGER
-abbr --add ss ss -nlput
-abbr --add ympv mpv --script-opts=ytdl_hook-ytdl_path=yt-dlp
-abbr --add gv nvim --listen /tmp/godot.pipe
-abbr --add r bin/rails
-abbr --add rd bin/dev
-abbr --add rc bin/rails console
-abbr --add kp keepassxc-cli
-abbr --add sql nvim -c DBUI
 abbr --add op opencode
 abbr --add doeach "fzf --multi | entr -rp"
 
@@ -172,10 +98,6 @@ if not contains $_asdf_shims $PATH
 end
 set --erase _asdf_shims
 
-if test -n "$fish_private_mode"
-  set -x STARSHIP_FISH_PRIVATE_MODE "$fish_private_mode"
-end
-
 if status is-interactive
   # Commands to run in interactive sessions can go here
   if command -vq atuin;
@@ -186,7 +108,6 @@ if status is-interactive
   end
 
   command -vq direnv; and direnv hook fish | source
-  command -vq starship; and starship init fish | source
   command -vq zoxide; and zoxide init fish | source
   command -vq fzf; and fzf --fish | FZF_CTRL_R_COMMAND= FZF_ALT_C_COMMAND= FZF_CTRL_T_COMMAND= source
 end
