3f8d87dbcbb5b65daf14747af96a3308df0e347b
Manuel Luna <manuellunada@gmail.com>
2026-05-16 00:12:41 -0600

    tmux mini.sh use tmux_fzf

--- .config/tmux/mini.sh
+++ .config/tmux/mini.sh
@@ -9,12 +9,13 @@ sed -e 's|\(.*plugins/tpm/tpm.*\)|# \1|g' \
   -e 's|run-shell .*search_forwards.sh.*$|command-prompt -T search -p "(search down)" { send-keys -X search-forward '\''%%'\'' }|g ' \
   -e 's|run-shell .*rename_window.*$|command-prompt -p "Rename window:" "rename-window -t . '\''%%'\''"|g ' \
   -e 's|run-shell .*session_new.sh.*$|command-prompt -p "New session:" "new-session -c ~ -s '\''%%'\''"|g ' \
+  -e 's|display.* .*switch_session.sh.*$|display-popup -h 10 -y S -EE ~/.local/bin/tmux_fzf.sh|g ' \
   -e 's|run-shell .*balance.sh.*$|select-layout -E|g ' \
   -e 's|^bind.*run-shell .*resize.sh.*$|bind Left resize-pane -t . -L 5\nbind Right resize-pane -t . -R 5\nbind Up resize-pane -t . -U 5\nbind Down resize-pane -t . -D 5|g ' \
   -e 's|run-shell .*reload.sh.*$|source-file ~/.tmux.conf \\; display-message "Config reloaded"|g ' \
   ~/.tmux.conf | \
   awk '{
-    if ($0 ~ /tmux_switch_session.sh/) {
+    if ($0 ~ /tmux_fzf.sh/) {
       print $0;
     } else if ($0 ~ /run-shell/) {
       msg = sprintf("display-message \"%s not implemented\"\n", $4);
