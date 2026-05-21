003fe10c9baeddeff0b261463fd95ee916ecd41a
Manuel Luna <manuellunada@gmail.com>
2026-05-21 15:46:56 -0600

    tmux_util.sh fix empty session

--- .local/scripts/bin/tmux_util.sh
+++ .local/scripts/bin/tmux_util.sh
@@ -47,13 +47,16 @@ get_sessions() {
 
   saved="$(get_saved_sessions)"
   live="$(get_live_sessions)"
-  if [ "$with_label" -eq 1 ]; then
-    saved_but_not_loaded="$(grep -vx "$live" <<< "$saved" | awk '{printf "%s (lazy)\n", $0}')"
+  if [ -n "$saved" ]; then
+    if [ "$with_label" -eq 1 ]; then
+      saved_but_not_loaded="$(grep -vx "$live" <<< "$saved" | awk '{printf "%s (lazy)\n", $0}')"
+    else
+      saved_but_not_loaded="$(grep -vx "$live" <<< "$saved")"
+    fi
+    printf "%s\n%s\n" "$live" "$saved_but_not_loaded" | sort -h | grep -Ev '^\s*$'
   else
-    saved_but_not_loaded="$(grep -vx "$live" <<< "$saved")"
+    echo "$live" | sort -h | grep -Ev '^\s*$'
   fi
-
-  printf "%s\n%s\n" "$live" "$saved_but_not_loaded" | sort -h | grep -Ev '^\s*$'
 }
 
 # MARKS
