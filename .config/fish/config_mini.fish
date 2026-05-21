f70a5812e8e701c848755908d36d6c0d3991e55f
Manuel Luna <manuellunada@gmail.com>
2026-05-21 15:48:14 -0600

    fish config_mini mkcdir

--- .config/fish/config_mini.fish
+++ .config/fish/config_mini.fish
@@ -79,6 +79,22 @@ abbr --add mv mv -i
 abbr --add op opencode
 abbr --add doeach "fzf --multi | entr -rp"
 
+function mkcdir
+  set -l dir "$argv[1]"
+  if test -z "$dir"
+    return 1
+  end
+
+  if test -d "$dir"
+    cd "$dir"; or return 1
+    return 0
+  end
+
+  mkdir "$dir"; or return 1
+
+  cd "$dir"
+end
+
 set fish_cursor_default block
 set fish_cursor_insert block blink
 set fish_cursor_replace_one underscore
