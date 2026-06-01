7b8fbbd457646e55bf9fd0ee702099070d04a07e
Manuel Luna <manuellunada@gmail.com>
2026-06-01 15:33:30 -0600

    fish tempdo function

---
+++ .config/fish/functions/tempdo.fish
@@ -0,0 +1,7 @@
+function tempdo --description "Creates a tmp directory in /tmp, drops you in a new shell, and removes the directory after you exit"
+  set -l dir (mktemp --directory /tmp/tmp.XXXXXXXX) ; or return
+  echo "New fish shell starting, type C-d or exit when done"
+  fish -C "cd $dir"
+  rm -rf "$dir" ; or return
+  echo "$dir" removed
+end
