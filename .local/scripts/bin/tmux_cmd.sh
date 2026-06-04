accd881395e168266d7e95c73567133ebab6200d
Manuel Luna <manuellunada@gmail.com>
2026-06-04 10:03:59 -0600

    tmux_cmd.sh include command-alias entries

--- .local/scripts/bin/tmux_cmd.sh
+++ .local/scripts/bin/tmux_cmd.sh
@@ -6,15 +6,18 @@ fi
 
 cleanup() {
   [ -n "$selected" ] && return
-  rm ~/.cache/tmux_cmd.sh.selected
+  rm ~/.cache/tmux_cmd.sh.selected &>/dev/null
 }
 trap cleanup EXIT
 
 selected="$(
-  tmux list-commands -F '#{command_list_name} #{command_list_usage}' | \
+  {
+    tmux list-commands -F '#{command_list_name} #{command_list_usage}' ;
+    tmux show-option -gv command-alias | awk -F= '{printf "%s (alias: %s)\n", $1, $2}' ;
+  } | \
   mina -nobanner -icon "" | \
   head -n 1 | \
-  awk '{print $1}'
+  awk '{ if ($0 ~ /\(alias: /) { sub(/.*\(alias: /, ""); sub(/\)$/, ""); print $0 } else { print $1 } }'
 )"
 [ -z "$selected" ] && exit 0
 
