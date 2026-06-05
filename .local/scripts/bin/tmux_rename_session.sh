f284f71dd6f278417162e143c75afed5e45e2312
Manuel Luna <manuellunada@gmail.com>
2026-06-05 02:49:18 -0600

    tmux_rename_session.sh check for dups

--- .local/scripts/bin/tmux_rename_session.sh
+++ .local/scripts/bin/tmux_rename_session.sh
@@ -6,4 +6,8 @@ current_name=$(tmux display -p -F "#{session_name}")
 name=$(tmux_ask "Rename session" "$current_name") || exit
 [ -z "$name" ] && exit 0
 
+if tmux has-session -t "$name"; then
+  tmux_alert Duplicate session
+  exit 0
+fi
 tmux rename-session -t . "$name"
