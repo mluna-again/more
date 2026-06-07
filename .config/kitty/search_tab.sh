b9c6e71e50ea094ccdfc1fbd098439df1e321cdf
Manuel Luna <manuellunada@gmail.com>
2026-06-07 17:14:23 -0600

    kitty fix exit in search_tab.sh

--- .config/kitty/search_tab.sh
+++ .config/kitty/search_tab.sh
@@ -3,11 +3,11 @@
 export PATH="$PATH:$HOME/.local/go/bin"
 if command -v mina &>/dev/null && command -v jq &>/dev/null; then
   selected="$(kitten @ ls | jq -r '.[0].tabs[] | "\(.id) \(.title)"' | mina -nth 1 -title "Search KITTY tab" -icon "" | awk '{print $1}')"
-  [ -z "$selected" ] && return
+  [ -z "$selected" ] && exit
 
   kitten @ focus-tab --match id:"$selected"
 else
   printf "[WARN]: mina/jq missing\n"
   printf "Press any key to exit."
-  read -N 1
+  read -r -N 1
 fi
