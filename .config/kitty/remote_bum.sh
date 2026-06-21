253ad8342f6f24ec3f3de40f3e1df208fcc79e75
Manuel Luna <manuellunada@gmail.com>
2026-06-21 12:34:28 -0600

    kitty remote_bum.sh

---
+++ .config/kitty/remote_bum.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+die() {
+  echo -e "$*" 1>&2
+  exit 1
+}
+
+kitty_window_cmdlines="$(kitten @ ls --match-tab state:active | jq '.[0].tabs[0].windows[].foreground_processes[0].cmdline')"
+ssh_cmd="$(echo "$kitty_window_cmdlines" | jq -r '. | select((. | length) > 1 and (.[0] == "ssh" or (.[1] | endswith("vm_ssh.sh")))) | "\(.[0]) \(.[1]) \(.[2])"')"
+ssh_cmd_lines="$(echo "$ssh_cmd" | wc -l)"
+if (( ssh_cmd_lines == 1 )) && [ -n "$ssh_cmd" ]; then
+  host="$(awk '{if (NF == 2) {print $2} else {print $3}}' <<< "$ssh_cmd")"
+  cmd="$(awk '{print $1}' <<< "$ssh_cmd")"
+  if [ "$cmd" = ssh ]; then
+    ssh -t "$host" -o RemoteCommand="bum -toggle"
+  else
+    vm_ssh.sh "$host" --cmd "bum -toggle"
+  fi
+else
+  die "Could not infer host to connect to. Expected a *single* window with a cmd with the following format: \`ssh <host>\` or \`bash ~/.local/scripts/bin/vm_ssh.sh <host>\`.\nFound:\n${ssh_cmd:-Nothing}"
+fi
