4e29c245af69692a03972c1f71b779d13323123a
Manuel Luna <manuellunada@gmail.com>
2026-06-21 12:37:17 -0600

    kitty remote_bum handle full ssh path

--- .config/kitty/remote_bum.sh
+++ .config/kitty/remote_bum.sh
@@ -6,7 +6,7 @@ die() {
 }
 
 kitty_window_cmdlines="$(kitten @ ls --match-tab state:active | jq '.[0].tabs[0].windows[].foreground_processes[0].cmdline')"
-ssh_cmd="$(echo "$kitty_window_cmdlines" | jq -r '. | select((. | length) > 1 and (.[0] == "ssh" or (.[1] | endswith("vm_ssh.sh")))) | "\(.[0]) \(.[1]) \(.[2])"')"
+ssh_cmd="$(echo "$kitty_window_cmdlines" | jq -r '. | select((. | length) > 1 and ((.[0] | endswith("ssh")) or (.[1] | endswith("vm_ssh.sh")))) | "\(.[0]) \(.[1]) \(.[2])"')"
 ssh_cmd_lines="$(echo "$ssh_cmd" | wc -l)"
 if (( ssh_cmd_lines == 1 )) && [ -n "$ssh_cmd" ]; then
   host="$(awk '{if (NF == 2) {print $2} else {print $3}}' <<< "$ssh_cmd")"
