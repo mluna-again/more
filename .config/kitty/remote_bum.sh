e1eaca5eb5409656d8d7b22446e01b5742851fc7
Manuel Luna <mluna881@proton.me>
2026-06-21 13:01:27 -0600

    pragmatism not idealism

--- .config/kitty/remote_bum.sh
+++ .config/kitty/remote_bum.sh
@@ -2,20 +2,20 @@
 
 die() {
   echo -e "$*" 1>&2
+  read -n 1
   exit 1
 }
 
 kitty_window_cmdlines="$(kitten @ ls --match-tab state:active | jq '.[0].tabs[0].windows[].foreground_processes[0].cmdline')"
-ssh_cmd="$(echo "$kitty_window_cmdlines" | jq -r '. | select((. | length) > 1 and ((.[0] | endswith("ssh")) or (.[1] | endswith("vm_ssh.sh")))) | join(" ")')"
+ssh_cmd="$(echo "$kitty_window_cmdlines" | jq -r '. | select((. | length) > 1 and ((.[0] | endswith("ssh")) or (.[1] | endswith("vm_ssh.sh")))) | join(" ")' | grep -v '_bum')"
 ssh_cmd_lines="$(echo "$ssh_cmd" | wc -l)"
 if (( ssh_cmd_lines == 1 )) && [ -n "$ssh_cmd" ]; then
   host="$(awk '{if (NF == 2) {print $2} else {print $3}}' <<< "$ssh_cmd")"
-  cmd="$(awk '{print $1}' <<< "$ssh_cmd")"
-  if [ "$cmd" = bash ]; then
-    vm_ssh.sh "$host" --cmd "bum -toggle"
-  else
-    ssh -t "$host" -o RemoteCommand="bum -toggle"
+  host="${host}_bum"
+  if ! grep -riEq "Host\s+$host" ~/.ssh/config ~/.ssh/config.d; then
+    die "No matching entry in ~/.ssh/config\nExpected a hostname named $host, none found.\nAdd an entry with the appropiate 'bum -toggle' RemoteCommand."
   fi
+  ssh "$host"
 else
   die "Could not infer host to connect to. Expected a *single* window with a cmd with the following format: \`ssh <host>\` or \`bash ~/.local/scripts/bin/vm_ssh.sh <host>\`.\nFound:\n${ssh_cmd:-Nothing}"
 fi
