b13b562445bd630bdf74c872da692dc5959ffc01
Manuel Luna <manuellunada@gmail.com>
2026-06-24 23:18:00 -0600

    tmux_stach.sh nvm

--- .local/scripts/bin/tmux_stack.sh
+++ .local/scripts/bin/tmux_stack.sh
@@ -1,17 +1,28 @@
 #! /usr/bin/env bash
 
 key="$1"
+read -r stacked_r < <(tmux display -p '#{@stack_at_right}')
+read -r stacked_l < <(tmux display -p '#{@stack_at_left}')
+read -r at_right at_left < <(tmux display -p '#{pane_at_right} #{pane_at_left}')
+
+should_resize=
+if [ "$stacked_l" = 1 ] && [ "$at_left" = 1 ]; then
+  should_resize=1
+elif [ "$stacked_r" = 1 ] && [ "$at_right" = 1 ]; then
+  should_resize=1
+fi
+
 case "$key" in
   STAY)
     tmux resize-pane -y "100%"
     ;;
   j)
     tmux select-pane -D
-    tmux resize-pane -y "100%"
+    [ -n "$should_resize" ] && tmux resize-pane -y "100%"
     ;;
   k)
     tmux select-pane -U
-    tmux resize-pane -y "100%"
+    [ -n "$should_resize" ] && tmux resize-pane -y "100%"
     ;;
 esac
 
