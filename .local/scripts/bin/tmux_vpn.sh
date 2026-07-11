c2b1f4e268113fb611fef63c22343b9b2648f006
Manuel Luna <manuellunada@gmail.com>
2026-07-11 12:13:39 -0600

    tmux_vpn.sh join multiple interfaces

--- .local/scripts/bin/tmux_vpn.sh
+++ .local/scripts/bin/tmux_vpn.sh
@@ -2,14 +2,38 @@
 
 command -v nmcli &>/dev/null || exit
 
-match="$(nmcli -f NAME,TYPE,ACTIVE con show --active | awk '$3 == "yes" {print $1, $2}' | grep -e vpn -e wireguard -e tun -e tap | awk '{print $1}')"
-if [ -n "$match" ] ; then
-  match_count="$(echo "$match" | wc -l)"
-  if (( match_count == 1 )) && [[ "$match" =~ ^.*tailscale.*$ ]]; then
-    if tailscale status --self &>/dev/null; then
-      echo "#[bg=#{@yellow},fg=#{@black2}] tailscale #[bg=default,fg=default] "
+join() {
+  local count=0 index=0 lines=() line sep="$1"
+
+  while read -r line; do
+    lines+=( "$line" )
+    (( count++ ))
+  done
+
+  while (( index < count )); do
+    printf "%s" "${lines[$index]}"
+    if (( index < ( count - 1 ) )); then
+      printf "%s" "$sep"
     fi
-  else
-    echo "#[bg=#{@yellow},fg=#{@black2}] $match #[bg=default,fg=default] "
-  fi
+    (( index++ ))
+  done
+}
+
+filter_tailscale() {
+  local line
+
+  while read -r line; do
+    if [[ "$line" =~ ^tailscale[0-9]*$ ]]; then
+      if tailscale status --browser=false &>/dev/null; then
+        echo tailscale
+      fi
+    else
+      echo "$line"
+    fi
+  done
+}
+
+matches="$(nmcli -f NAME,TYPE,ACTIVE con show --active | awk '$3 == "yes" {print $1, $2}' | grep -e vpn -e wireguard -e tun -e tap | awk '{print $1}' | filter_tailscale | join /)"
+if [ -n "$matches" ] ; then
+  echo "#[bg=#{@yellow},fg=#{@black2}] $matches #[bg=default,fg=default] "
 fi
