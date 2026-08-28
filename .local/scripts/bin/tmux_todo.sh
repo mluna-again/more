4355ccbc291564800633090af34884542a4ddb46
Manuel Luna <manuellunada@gmail.com>
2026-08-27 20:08:55 -0600

    tmux window_msg_styles

--- .local/scripts/bin/tmux_todo.sh
+++ .local/scripts/bin/tmux_todo.sh
@@ -2,6 +2,36 @@
 
 if [ -z "$1" ]; then
   tmux set-option -wu @window_msg
+  tmux set-option -wu @window_msg_styles
 else
-  tmux set-option -w @window_msg "[$1] "
+  case "${1,,}" in
+    blocked)
+      tmux set-option -w @window_msg_styles ",bg=red,fg=black"
+      tmux set-option -w @window_msg " "
+      ;;
+    done)
+      tmux set-option -w @window_msg_styles ",bg=green,fg=black"
+      tmux set-option -w @window_msg " "
+      ;;
+    wip)
+      tmux set-option -w @window_msg_styles ",bg=blue,fg=black"
+      tmux set-option -w @window_msg " "
+      ;;
+    todo)
+      tmux set-option -w @window_msg_styles ",bg=colour242,fg=white"
+      tmux set-option -w @window_msg " "
+      ;;
+    draft)
+      tmux set-option -w @window_msg_styles ",bg=cyan,fg=black"
+      tmux set-option -w @window_msg "󰟶 "
+      ;;
+    review)
+      tmux set-option -w @window_msg_styles ",bg=yellow,fg=black"
+      tmux set-option -w @window_msg " "
+      ;;
+    ready)
+      tmux set-option -w @window_msg_styles ",bg=colour23,fg=white"
+      tmux set-option -w @window_msg " "
+      ;;
+  esac
 fi
