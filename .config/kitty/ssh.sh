e34efea7416002b6d90ea894401ccfcfcc8b0184
Manuel Luna <manuellunada@gmail.com>
2026-09-03 22:51:42 -0600

    kitty ssh.sh uniq

--- .config/kitty/ssh.sh
+++ .config/kitty/ssh.sh
@@ -7,7 +7,7 @@ if ! command -v mina &>/dev/null; then
   exit 1
 fi
 
-host="$(grep -r -h -i "host\s" ~/.ssh/config ~/.ssh/config.d | awk '$2 != "*" {print $2}' | tr -d ' ' | mina -title "SSH in new tab" -icon "")"
+host="$(grep -r -h -i "host\s" ~/.ssh/config ~/.ssh/config.d | awk '$2 != "*" {print $2}' | tr -d ' ' | sort | uniq | mina -title "SSH in new tab" -icon "")"
 [ -z "$host" ] && exit 1
 
 kitten @ launch --type=tab --location=after --title="$host" ssh "$host"
