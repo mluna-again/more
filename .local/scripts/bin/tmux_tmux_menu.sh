fe4eabae695165a40e966073a690205c5b8f3c21
Manuel Luna <manuellunada@gmail.com>
2026-09-06 12:22:00 -0600

    tmux_tmux_menu.sh splits

--- .local/scripts/bin/tmux_tmux_menu.sh
+++ .local/scripts/bin/tmux_tmux_menu.sh
@@ -6,10 +6,13 @@ lsess_pref=""
 
 tmux display-menu -x 0 -y S -M -O -- \
   "${lsess_pref}Last session (${lsess:-<empty>})" "" "switch-client -l" \
-  "-------" "" "" \
+  "-                      " "" "" \
+  "Split Vertical (v)" "" "split-window -c '#{pane_current_path}' -h" \
+  "Split Horizontal (s)" "" "split-window -c '#{pane_current_path}' -v" \
+  "-                      " "" "" \
   "Change prefix" "" "run-shell ~/.local/scripts/bin/tmux_toggle_prefix.sh" \
   "Toggle borders" "" "run-shell ~/.local/scripts/bin/tmux_toggle_panel_borders.sh" \
   "Kill current pane program" "" "run-shell ~/.local/scripts/bin/tmux_kill_fg.sh" \
   "Kill current pane" "" "kill-pane -t ." \
-  "-------" "" "" \
+  "-                      " "" "" \
   "Detach" "" "detach-client"
